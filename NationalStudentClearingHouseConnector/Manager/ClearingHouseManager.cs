dd678c6464a6d7814989ade6c410883f6366eb0a
PaulBranham <branhap@gmail.com>
2018-08-17 23:28:45 -0600

    Initial Commit of Project.

---
+++ NationalStudentClearingHouseConnector/Manager/ClearingHouseManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace NationalStudentClearingHouse.ETranscriptsConnector.Manager
+{
+    public class ClearingHouseManager
+    {
+        private ClearingHouseConfig config;
+
+        public ClearingHouseManager (ClearingHouseConfig schoolConfig)
+        {
+            config = schoolConfig;
+        }
+
+        public ClearingHouseManager()
+        {
+            config = new ClearingHouseConfig();
+        }
+
+        /// <summary>
+        /// Gets the URL to send the student to when they attempt to authenticate.
+        /// </summary>
+        /// <param name="studentId"></param>
+        public string GetAuthenticationResponse(string studentId)
+        {
+            var hwr = (HttpWebRequest)WebRequest.Create(config.Link);
+            hwr.AllowAutoRedirect = true;
+            hwr.MaximumAutomaticRedirections = 5;
+            hwr.ContentType = config.ContentType;
+            hwr.Referer = config.Referer;
+            hwr.UserAgent = config.UserAgent;
+            hwr.Method = config.RequestMethod;
+
+            Stream requestStream = hwr.GetRequestStream();
+            StreamWriter streamWriter = new StreamWriter(requestStream);
+            streamWriter.Write(GenerateWebRequestBody(studentId));
+            streamWriter.Close();
+
+            var webResponse = hwr.GetResponse();
+            var streamResponse = webResponse.GetResponseStream();
+            var streamReader = new StreamReader(streamResponse);
+
+            var response = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            return response;
+        }
+
+        private string GenerateWebRequestBody(string studentId)
+        {
+            return "user_id=" + config.UserName + "&password=" + config.Password + "&id=" + studentId + "&pathtype=" + config.PathType;
+        }
+    }
+}
---
+++ NationalStudentClearingHouseConnector/Models/ClearingHouseConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NationalStudentClearingHouse.ETranscriptsConnector
+{
+    public class ClearingHouseConfig
+    {
+        /// <summary>
+        /// User name assigned to School.
+        /// </summary>
+        public string UserName { get; set; } = "username";
+
+        /// <summary>
+        /// Password that allows school to log into National Student Clearing House.
+        /// </summary>
+        public string Password { get; set; } = "password";
+
+        /// <summary>
+        /// Web App that is sending the request.
+        /// </summary>
+        public string Referer { get; set; } = "https://example.edu/eTransConnector";
+
+        /// <summary>
+        /// Setting provided by National Student Clearing House and passed to them.
+        /// </summary>
+        public string PathType { get; set; } = "P";
+
+        /// <summary>
+        /// URL to send the request to.
+        /// </summary>
+        public string Link { get; set; } = "https://secure.studentclearinghouse.org/sssportalui/authenticate";
+
+        /// <summary>
+        /// The Content Type for the request.
+        /// </summary>
+        public string ContentType { get; set; } = "application/x-www-form-urlencoded;";
+
+        /// <summary>
+        /// Web Request User Agent
+        /// </summary>
+        public string UserAgent { get; set; } = "msie 5.5";
+
+        public string RequestMethod { get; set; } = "POST";
+    }
+}
